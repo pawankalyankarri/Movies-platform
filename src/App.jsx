64a48cf501959f311e1d797f96b0b05a8eae8639
pawankalyan <pawankalyankarri22@gmail.com>
2025-08-03 00:24:59 +0530

    components organized

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import HomePage from './components/home/HomePage'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Head from './components/home/Head'
+import Sidebar from './components/home/Sidebar'
+import MainPage from './components/home/Mainpage'
+import AddMovies from './components/sidebar/AddMovies'
 
 function App() {
 
   return (
-    <div className='apppage'>
-      <HomePage/>
+    <div className='apppage homepage'>
+        
+            
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/addmovie' element={<AddMovies/>} />
+        {/* <Route path="/head" element={<Head />} />
+        <Route path="/" element={<Sidebar />} />
+        <Route path="/main" element={<MainPage />} /> */}
+      </Routes>
+
     </div>
       )
 }
--- src/components/home/Mainpage.jsx
+++ src/components/home/Mainpage.jsx
@@ -3,8 +3,9 @@ import DisplayVideo from "../main/DisplayVideo";
 const MainPage = () => {
     return(
         <main className="mainpage">
-            <DisplayVideo/>
-            {/* <h4>main page</h4> */}
+            {/* <DisplayVideo/> */}
+            <h4>main page</h4>
+            <p>here we need to carts</p>
         </main>
     )
 }
