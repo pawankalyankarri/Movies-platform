e38559680ebddd5be43a0c6f812a627d7c598259
pawankalyan <pawankalyankarri22@gmail.com>
2025-08-06 23:56:54 +0530

    movies presenting in the movies page

--- src/components/allmovies/AllMovies.jsx
+++ src/components/allmovies/AllMovies.jsx
@@ -1,37 +1,38 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import '../../styles/sidebar/allmovies.css'
 
-const AllMovies = () =>{
 
-    let [movies,setMovies] = useState([])
-    useEffect(() => {
+const AllMovies = () => {
+  let [movies, setMovies] = useState([]);
+  useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/movie/getmovies")
       .then((res) => {
         console.log(res);
-        setMovies(res.data)
+        setMovies(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
-  
-    return (
-        <div>
-            <h1>all movies</h1>
-            {movies.map(movie=>{
-                return(
-                    <div key={movie.mid}>
-                        <h2>{movie.mname}</h2>
-                        <img src={`http://127.0.0.1:8000/${movie.mimg}`} alt="img" /> <br />
-                        <video controls autoPlay >
-                            <source src={`http://127.0.0.1:8000/${movie.mvideo}`} type="video/mp4"/>
-                        </video>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
 
-export default AllMovies
+  return (
+    <div className="allmovies">
+      {movies.map((movie) => {
+        return (
+          <div key={movie.mid} className="row moviesinfo">
+            <div className=" imgcard col-4">
+                <div className="img">
+                    <img src={`http://127.0.0.1:8000/${movie.mimg}`} alt="img" /> 
+                </div>
+            </div>
+            
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AllMovies;
