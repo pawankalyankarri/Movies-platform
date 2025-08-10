1bdd03e0ae06faf5d7b1dcb0d445857fc1b5524c
pawankalyan <pawankalyankarri22@gmail.com>
2025-08-11 00:17:50 +0530

    sidebar updated

--- src/components/home/Mainpage.jsx
+++ src/components/home/Mainpage.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import DisplayVideo from "../main/DisplayVideo";
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import TeluguMovies from "../sidebar/TeluguMovies";
 
 const MainPage = () => {
+    useEffect(()=>{
+        axios.get("http://127.0.0.1:8000/movie/getmovies").then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.log(err)
+        })
+    },[])
     return(
         <main className="mainpage">
             {/* <DisplayVideo/> */}
             <h4>main page</h4>
             <p>here we need to carts</p>
+            <Routes>
+                <Route path="/telugum" element={<TeluguMovies/>}/>
+            </Routes>
         </main>
     )
 }
--- src/components/main/PlayMovie.jsx
+++ src/components/main/PlayMovie.jsx
@@ -1,8 +1,13 @@
-const PlayMovie = (props) => {
-    console.log(props.mvideo)
+import { useLocation } from "react-router-dom"
+import '../../styles/sidebar/allmovies.css'
+
+const PlayMovie = () => {
+    let location = useLocation()
+    let movie = location.state
+    console.log(movie)
     return (
-        <div>
-            <video src={`http://127.0.0.1:8000/${props.mvideo}`} autoPlay controls ></video>
+        <div className="playMovie">
+            <video src={`http://127.0.0.1:8000/${movie}`} autoPlay controls ></video>
         </div>
     )
 }
